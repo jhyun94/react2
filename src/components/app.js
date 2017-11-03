fdd2957bebf9f6a6fe6854cf38852f017f51e297
jhyun94 <hyunjonathan@gmail.com>
2017-11-03 14:17:42 -0500

    created activebook container and connected to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import BookList from '../containers/book-list';
+import ActiveBook from '../containers/active-book';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <ActiveBook />
       </div>
     );
   }
---
+++ src/containers/active-book.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+
+export default class ActiveBook extends Component{
+
+  render(){
+    return(
+      <div>ActiveBook!!</div>
+    )
+  }
+}
